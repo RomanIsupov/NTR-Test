0035b70c9865e772b051b450a85ab3a5b5df10ea
RomanIsupov <romisup@gmail.com>
2022-07-15 21:29:31 +0800

    Initial commit

---
+++ src/main/java/ru/romanisupov/ntrtest/NtrTestApplication.java
@@ -0,0 +1,13 @@
+package ru.romanisupov.ntrtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NtrTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NtrTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/romanisupov/ntrtest/NtrTestApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.romanisupov.ntrtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NtrTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
